877e7b70f6b67647d35ee5e8988ed71bc4c83dd0
davidliang781205 <davidliang781205@gmail.com>
2017-04-11 21:33:21 +0000

    Completed ar_exercises

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ActiveRecord::Base
-
+  validates_presence_of :first_name, :last_name, :store_id
+  validates_inclusion_of :hourly_rate, in: 40..200
+  belongs_to :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
+  validates_length_of :name, minimum: 3
+  validates_numericality_of :annual_revenue
+  validate :men_or_women
+  has_many :employees
 
+  def men_or_women
+    if !(mens_apparel) && !(womens_apparel)
+      errors.add(:must_be_men_or_women, "Store must carry at least one of the men's or women's apparel")
+    end
+  end
 end
